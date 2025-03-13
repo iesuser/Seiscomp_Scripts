094f39316cc550e78ede16f8718c4c31e59de1ad
R-Grigala <r.grigalashvili777@gmail.com>
2025-03-13 17:24:02 +0400

    add xml_path

--- import_eq2iesdata/import_eq2.py
+++ import_eq2iesdata/import_eq2.py
@@ -1,9 +1,42 @@
-"""
+#!/usr/bin/env python3
+import os
+import sys
+import re
+import xml.etree.ElementTree as ET
+import subprocess
+import shutil  # Import the shutil module
 
-გვჭირდება სკრიპტის სამი ნაწილი
-1) XML ფაილის დაგენერირება.
-2) XML ფაილის სწორად გაპარსვა და საჭირო ინფორმაციის ამოღება
-3) XML ფაილიდან ამოღებული ინფორმაციის გადაცემა PHP ფაილისთვის
+server_ip = 'localhost'
+# Define the desired zoom level (adjust as needed)
+zoom_level = 0
+script_path = os.path.dirname(os.path.abspath(__file__))
+event_id = sys.argv[2]
 
-"""
 
+def xml_dump(xml_path, event_id, server_ip):
+    # seiscomp exec scxmldump -APMfmp -o /home/sysop/Code/Seiscomp_Scripts/import_eq2iesdata/temp/eq_log.xml -E grg2025fary -d localhost
+    command = f'seiscomp exec scxmldump -APMfmp -o {xml_path} -E {event_id} -d {server_ip}'
+    try:
+        subprocess.run(command, check=True, shell=True )
+    except subprocess.CalledProcessError as e:
+        print("Error:", e)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    """
+        გვჭირდება სკრიპტის სამი ნაწილი
+        1) XML ფაილის დაგენერირება.
+        2) XML ფაილის სწორად გაპარსვა და საჭირო ინფორმაციის ამოღება
+        3) XML ფაილიდან ამოღებული ინფორმაციის გადაცემა PHP ფაილისთვის
+    """
+
+    # გაშვებული სკრიპტის მისამართი
+    temp_dir_path = script_path + "/temp"
+    if not os.path.isdir(temp_dir_path):
+        os.mkdir(temp_dir_path)
+
+    xml_path = temp_dir_path + "/eq_log.xml"
+    html_file_path = temp_dir_path + "/redirectPostEq.html"
+
+    xml_dump(xml_path, event_id, server_ip)
