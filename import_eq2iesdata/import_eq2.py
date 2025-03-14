8fabbf94ff83f73e960533c99fa884652d0e5b8c
varsimashviliLuka <varsimashvili.official@gmail.com>
2025-03-14 14:26:41 +0400

    made changes in import_eq2

--- import_eq2iesdata/import_eq2.py
+++ import_eq2iesdata/import_eq2.py
@@ -6,9 +6,44 @@ import xml.etree.ElementTree as ET
 import subprocess
 import shutil  # Import the shutil module
 
-server_ip = 'localhost'
-script_path = os.path.dirname(os.path.abspath(__file__))
-event_id = sys.argv[2]
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+SERVER_IP = 'localhost'
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+EVENT_ID = sys.argv[2]
+
+# გაშვებული სკრიპტის მისამართი
+TEMP_DIR_PATH = SCRIPT_PATH + "/temp"
+if not os.path.isdir(TEMP_DIR_PATH):
+    os.mkdir(TEMP_DIR_PATH)
+
+LOGS_DIR_PATH = SCRIPT_PATH + "/logs"
+if not os.path.isdir(LOGS_DIR_PATH):
+    os.mkdir(LOGS_DIR_PATH)
+
+XML_PATH = TEMP_DIR_PATH + "/eq_log.xml"
+HTML_FILE_PATH = TEMP_DIR_PATH + "/redirectPostEq.html"
+
+SOFTWARE_NAMES = {'LOCSAT':'LocSAT(Seiscomp)'}
+
+FORM_LIST = []
+
+# Set up logging for the scheduler
+LOG_FILENAME = f'{LOGS_DIR_PATH}/import_eq2.log'
+MAX_LOG_SIZE = 5 * 1024 * 1024  # 5 MB
+BACKUP_COUNT = 3  # Keep 3 backup log files
+
+# Create a rotating file handler
+rotating_handler = RotatingFileHandler(LOG_FILENAME, maxBytes=MAX_LOG_SIZE, backupCount=BACKUP_COUNT)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+rotating_handler.setFormatter(formatter)
+
+# Set up the root logger
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.addHandler(rotating_handler)
 
 
 def xml_dump(xml_path, event_id, server_ip):
@@ -20,6 +55,39 @@ def xml_dump(xml_path, event_id, server_ip):
         print("Error:", e)
         sys.exit(1)
 
+# ეს ფუნქცია სეისკომპიდან მიღებულ დროს გადაიყვანს SHM-ის დროის ფორმატში
+# მაგ: "2017-12-17T23:26:41.96Z" გადავა შემდეგი სახით : '17-DEC-2017_23:26:41.960'
+def convert_seiscomp_time_to_shm_time(time):
+	
+	months = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
+	seiscomp_msec = time[20:23]
+	seiscomp_msec_splited = seiscomp_msec.split('Z')
+	a = seiscomp_msec_splited[0]	
+	year = time[:4]
+	day = time[8:10]
+	month = time[5:7]
+	HH = time[11:13]
+	MM = time[14:16]
+	ss = time[17:19]
+	msec = a.ljust(3,'0')
+	return day + "-" + months[int(month)-1] + "-" + year + "_"  + HH + ':' + MM + ':' + ss + '.' + msec
+
+# ფუნქცია აგენერირებს input-ებს მიწოდებული name(სახელი) და value(მნიშვნელობა) მნიშვნელობებით
+def generete_input(name, value, func_name='generete_input'):
+    FORM_LIST.append("<input name='" + str(name) + "' type='text' value='" + str(value) + "' />")
+    logging.debug(f'<{func_name} - name: {name}; value: {value}>')
+
+# ფუნქცია აგენერირებს input-ებს
+def smart_generete_input(name, element,first_child, second_child, round_number = None, convert_time_format = False):
+	
+    if element.find(first_child) is not None and element.find(first_child).find(second_child) is not None:
+        value = element.find(first_child).find(second_child).text
+        if round_number is not None:
+            value = round(float(value), round_number)
+        if convert_time_format:
+            value = convert_seiscomp_time_to_shm_time(value)
+
+        generete_input(name, value, func_name='smart_generete_input')
 
 if __name__ == '__main__':
     """
@@ -28,14 +96,79 @@ if __name__ == '__main__':
         2) XML ფაილის სწორად გაპარსვა და საჭირო ინფორმაციის ამოღება
         3) XML ფაილიდან ამოღებული ინფორმაციის გადაცემა PHP ფაილისთვის
     """
-
-    # გაშვებული სკრიპტის მისამართი
-    temp_dir_path = script_path + "/temp"
-    if not os.path.isdir(temp_dir_path):
-        os.mkdir(temp_dir_path)
-
-    xml_path = temp_dir_path + "/eq_log.xml"
-    html_file_path = temp_dir_path + "/redirectPostEq.html"
-
     # 1) XML ფაილის დაგენერირება სეისკომპიდან scxmldump-ის გამოყენებით .
-    xml_dump(xml_path, event_id, server_ip)
+    xml_dump(XML_PATH, EVENT_ID, SERVER_IP)
+    file = open(XML_PATH, "r")
+
+    xml_file_content = xmlstring = re.sub(' xmlns="[^"]+"', '', file.read(), count=1)
+
+    #String დან xml-ის "გაპარსვა"
+    root = ET.fromstring(xml_file_content)
+    #xml - ში origin ტეგის წაკითხვა
+    eventParameters_element =  root.find('EventParameters')
+    origin_element = eventParameters_element.find('origin')
+
+    # მიწისძვრის პარამეტრების წაკითხვა და input-ების დაგენერირება
+    #ამ ეტაპზე მოწმდება მიწისძვრა დათვლილია თუ არა რაიმე მეთოდით (მაგ:LOCSAT)
+    if origin_element.find('methodID') is not None:
+        soft = origin_element.find('methodID').text
+        for key, value in SOFTWARE_NAMES.items():
+            if key == soft:
+                soft = value
+                break
+    else:
+        soft = 'Raw'
+
+    logging.debug(f'soft variable - {soft}')
+
+    generete_input("soft", soft )
+
+    smart_generete_input("EQ_time", origin_element, 'time', 'value', convert_time_format = True)
+    smart_generete_input("EQ_rms", origin_element, 'time', 'uncertainty', 3)
+    # generete_input("EQ_rms", round(float(origin_element.find('time').find('uncertainty').text), 3 ))
+
+    smart_generete_input("EQ_latitude", origin_element, 'latitude', 'value', 4)
+    smart_generete_input("EQ_err_lat", origin_element, 'latitude', 'uncertainty', 3)
+    # generete_input("EQ_latitude", round(float(origin_element.find('latitude').find('value').text), 4 ))
+    # generete_input("EQ_err_lat", round(float(origin_element.find('latitude').find('uncertainty').text), 3))
+
+    smart_generete_input("EQ_longitude", origin_element, 'longitude', 'value', 4)
+    smart_generete_input("EQ_err_long", origin_element, 'longitude', 'uncertainty', 3)
+    # generete_input("EQ_longitude", round(float(origin_element.find('longitude').find('value').text), 4 ))
+    # generete_input("EQ_err_long", round(float(origin_element.find('longitude').find('uncertainty').text), 3 ))
+
+    smart_generete_input("EQ_depth", origin_element, 'depth', 'value', 3)
+    smart_generete_input("EQ_err_depth", origin_element, 'depth', 'uncertainty', 3)
+    # generete_input("EQ_depth", round(float(origin_element.find('depth').find('value').text), 3 ))
+    # generete_input("EQ_err_depth", round(float(origin_element.find('depth').find('uncertainty').text), 3))
+
+    smart_generete_input("EQ_phases_count", origin_element, 'quality', 'associatedPhaseCount', 3)
+    smart_generete_input("EQ_phases_used_count", origin_element, 'quality', 'usedPhaseCount', 3)
+    # generete_input("EQ_phases_count", origin_element.find('quality').find('associatedPhaseCount').text)
+    # generete_input("EQ_phases_used_count", origin_element.find('quality').find('usedPhaseCount').text)
+
+    smart_generete_input("EQ_station_used_count", origin_element, 'quality', 'usedStationCount')
+    # generete_input("EQ_station_used_count", origin_element.find('quality').find('usedStationCount').text )
+
+    smart_generete_input("EQ_max_azimuthal_gap", origin_element, 'quality', 'azimuthalGap', 3)
+    # smart_generete_input("minHorizontalUncertainty",origin_element,'uncertainty','minHorizontalUncertainty')
+
+    #გასარკვევია 
+    # if origin_element.find('uncertainty').find('minHorizontalUncertainty') is not None:
+    # 	min_horizontal_ancertaint = origin_element.find('uncertainty').find('minHorizontalUncertainty').text
+    # 	max_horizontal_ancertaint = origin_element.find('uncertainty').find('maxHorizontalUncertainty').text
+    # 	avarage = float(min_horizontal_ancertaint) + float(max_horizontal_ancertaint) / 2
+    # 	avarage1 = math.sqrt((float(min_horizontal_ancertaint) * 2 ) + (float(max_horizontal_ancertaint) * 2 ))
+    # 	print('aris')
+    # 	print('min_horizontal_ancertainty : ',origin_element.find('uncertainty').find('minHorizontalUncertainty').text)
+    # 	print('max_horizontal_ancertainty : ',origin_element.find('uncertainty').find('maxHorizontalUncertainty').text)
+    # 	print(avarage)
+    # 	print(avarage1)
+    # else:
+    # 	print('ar aris')
+
+    print(FORM_LIST)
+
+
+
+    
